44c946a7e0dea72c3048aa977e572f414eb62e23
Renato Natanael <renatonathan6@gmail.com>
2021-07-08 19:52:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,93 @@
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+// Testimonials Slider/Carrousel/SWIPER
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true,
+    breakpoints: {
+      767:{
+        slidesPerView: 2,
+        setWrapperSize: true
+      }
+    }
+})
+
+// SCROLLREVEAL: Mostrar elementos quando der scroll na página
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700,
+    reset: true
+})
+
+scrollReveal.reveal(`
+    #home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+    `,{ interval: 100 }
+)
+
+// Botão voltar para o topo
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  window.addEventListener('scroll', function() {
+    if(window.scrollY >= 560){
+      backToTopButton.classList.add('show')
+    }else{
+      backToTopButton.classList.remove('show')
+    }
+  })
+
+}
+// MENU ATIVO CONFORME A SEÇÃO VISIVEL NA PAGINA
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuCurrentSection() {
+
+}
+
+// WHEN SCROLL
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuCurrentSection()
+})
